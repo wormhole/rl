313ab041b236f83463ad5b9e0a1c0d6b8df35391
wormhole <363408268@qq.com>
2020-07-18 12:33:04 +0800

    breakout_ram.py

--- 打砖块/breakout_ram.py
+++ 打砖块/breakout_ram.py
@@ -1,14 +1,30 @@
 import os
+import random
+import time
+from collections import deque
 
 import gym
 import numpy as np
 import torch
 import torch.nn as nn
 
+# 超参数
+batch_size = 32
+lr = 0.0001
+init_epsilon = 0.1
+final_epsilon = 0.001
+gamma = 0.99
+target_update_step = 100
+memory_capacity = 10000
+episode = 50000
+episode_step = 2000
+observe = 1000
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Net(nn.Module):
+
+class QNet(nn.Module):
     def __init__(self, n_states, n_actions):
-        super(Net, self).__init__()
+        super(QNet, self).__init__()
         self.fc1 = nn.Sequential(
             nn.Linear(n_states, 256),
             nn.ReLU()
@@ -31,116 +47,137 @@ class Net(nn.Module):
         return x
 
 
-class DQN(object):
-    def __init__(self, n_states, n_actions, memory_capacity, target_update_step, batch_size, lr, epsilon, gamma):
-        self.epsilon = epsilon
-        self.lr = lr
+class DQNAgent(object):
+    def __init__(self, n_states, n_actions):
         self.n_states = n_states
         self.n_actions = n_actions
-        self.memory_capacity = memory_capacity
-        self.target_update_step = target_update_step
-        self.batch_size = batch_size
-        self.gamma = gamma
+        self.time_step = 0
+        self.epsilon = init_epsilon
+        self.replay_memory = deque()
 
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.eval_net, self.target_net = Net(n_states, n_actions).to(self.device), Net(n_states, n_actions).to(
-            self.device)
-        self.learn_step_counter = 0  # 用于 target 更新计时
-        self.memory_counter = 0  # 记忆库记数
-        self.memory = np.zeros((memory_capacity, n_states * 2 + 2))  # 初始化记忆库
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=lr)  # torch 的优化器
-        self.loss_func = nn.MSELoss()  # 误差公式
+        self.q_net = QNet(n_states, n_actions).to(device)
+        self.target_q_net = QNet(n_states, n_actions).to(device)
+
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr=lr)
+        self.loss_func = nn.MSELoss()
 
     def save(self):
-        torch.save(self.eval_net.state_dict(), "params.pth")
+        torch.save(self.q_net.state_dict(), "params.pth")
 
     def load(self):
         if os.path.exists("params.pth"):
-            self.eval_net.load_state_dict(torch.load("params.pth"))
-            self.target_net.load_state_dict(torch.load("params.pth"))
+            self.q_net.load_state_dict(torch.load("params.pth"))
+            self.target_q_net.load_state_dict(torch.load("params.pth"))
 
-    def choose_action(self, x):
-        x = torch.unsqueeze(torch.FloatTensor(x), 0).to(self.device)
-        if np.random.uniform() < self.epsilon:  # 选最优动作
-            actions_value = self.eval_net.forward(x)
-            action = torch.max(actions_value, 1)[1].cpu().numpy()[0]
-        else:  # 选随机动作
+    def choose_action(self, state):
+        state = torch.FloatTensor([state]).to(device)
+        if np.random.uniform() > self.epsilon:
+            q_value = self.q_net.forward(state)
+            action = torch.max(q_value, 1)[1].cpu().numpy()[0]
+        else:
             action = np.random.randint(0, self.n_actions)
+
+        if self.epsilon != 1 and self.epsilon > final_epsilon and self.time_step <= observe + episode:
+            self.epsilon -= (init_epsilon - final_epsilon) / episode
         return action
 
-    def store(self, s, a, r, s_):
-        mem = np.hstack((s, [a, r], s_))
-        # 如果记忆库满了, 就覆盖老数据
-        index = self.memory_counter % self.memory_capacity
-        self.memory[index, :] = mem
-        self.memory_counter += 1
+    def store(self, state, action, reward, _state, done):
+        self.replay_memory.append((state, action, reward, _state, done))
+        if len(self.replay_memory) > memory_capacity:
+            self.replay_memory.popleft()
 
-    def learn(self):
-        # target net 参数更新
-        if self.learn_step_counter % self.target_update_step == 0:
-            self.target_net.load_state_dict(self.eval_net.state_dict())
-        self.learn_step_counter += 1
+        if self.time_step > observe:
+            self.train()
+        self.time_step += 1
 
-        # 抽取记忆库中的批数据
-        sample_index = np.random.choice(self.memory_capacity, self.batch_size)
-        b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :self.n_states]).to(self.device)
-        b_a = torch.LongTensor(b_memory[:, self.n_states:self.n_states + 1].astype(int)).to(self.device)
-        b_r = torch.FloatTensor(b_memory[:, self.n_states + 1:self.n_states + 2]).to(self.device)
-        b_s_ = torch.FloatTensor(b_memory[:, -self.n_states:]).to(self.device)
+    def train(self):
+        batch = random.sample(self.replay_memory, batch_size)
+        state_batch = [data[0] for data in batch]
+        action_batch = [data[1] for data in batch]
+        reward_batch = [data[2] for data in batch]
+        _state_batch = [data[3] for data in batch]
+        done_batch = [data[4] for data in batch]
 
-        # 针对做过的动作b_a, 来选 q_eval 的值, (q_eval 原本有所有动作的值)
-        q_eval = self.eval_net(b_s).gather(1, b_a)
-        q_next = self.target_net(b_s_).detach()
-        q_target = b_r + self.gamma * q_next.max(1)[0].view(self.batch_size, 1)
-        loss = self.loss_func(q_eval, q_target)
+        state_batch = torch.FloatTensor(state_batch).to(device)
+        action_batch = torch.LongTensor(action_batch).view(batch_size, 1).to(device)
+        reward_batch = torch.FloatTensor(reward_batch).view(batch_size, 1).to(device)
+        _state_batch = torch.FloatTensor(_state_batch).to(device)
 
+        q_value = self.q_net(state_batch).gather(1, action_batch)
+        target_q_value = self.target_q_net(_state_batch).detach()
+        target_q_value = target_q_value.max(1)[0].view(batch_size, 1)
+        for i in range(batch_size):
+            if done_batch[i]:
+                target_q_value[i][0] = reward_batch[i]
+            else:
+                target_q_value[i][0] = reward_batch[i] + gamma * target_q_value[i][0]
+
+        loss = self.loss_func(q_value, target_q_value)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+        if self.time_step % target_update_step == 0:
+            self.target_q_net.load_state_dict(self.q_net.state_dict())
+            self.save()
 
-# 超参数
-BATCH_SIZE = 32
-LR = 0.0001  # 学习率
-EPSILON = 0.9  # 最优选择动作百分比
-GAMMA = 0.99  # 奖励递减参数
-TARGET_UPDATE_STEP = 100  # Q 现实网络的更新频率
-MEMORY_CAPACITY = 10000  # 记忆库大小
-EPISODE = 100000
-EPISODE_STEP = 2000
-LEARN_START = 1000
+
+def train(env, agent):
+    total_reward = 0
+    for ep in range(1, episode + 1):
+        state = env.reset()
+        state = state / 255.0
+        ep_reward = 0
+        for step in range(episode_step):
+            env.render()
+            action = agent.choose_action(state)
+
+            # 执行动作
+            _state, reward, done, info = env.step(action)
+            _state = _state / 255.0
+            agent.store(state, action, reward, _state, done)
+
+            ep_reward += reward
+            if done:
+                break
+            state = _state
+        print("episode: ", ep, "reward: ", ep_reward, "epsilon: ", agent.epsilon)
+        total_reward += ep_reward
+        if ep % 100 == 0:
+            print("av_reward(100): ", total_reward / 100)
+            total_reward = 0
+
+
+def test(env, agent):
+    agent.load()
+    agent.epsilon = 1
+    total_reward = 0
+    for ep in range(1, episode + 1):
+        state = env.reset()
+        state = state / 255.0
+        ep_reward = 0
+        for step in range(episode_step):
+            time.sleep(0.05)
+            env.render()
+            action = agent.choose_action(state)
+
+            # 执行动作
+            _state, reward, done, info = env.step(action)
+
+            ep_reward += reward
+            if done:
+                break
+            state = _state
+        print("episode: ", ep, "reward: ", ep_reward)
+        total_reward += ep_reward
+        if ep % 100 == 0:
+            print("av_reward(100): ", total_reward / 100)
+            total_reward = 0
+
 
 if __name__ == "__main__":
     env = gym.make("Breakout-ram-v4")
-    N_ACTIONS = env.action_space.n  # 动作数量
-    N_STATES = env.observation_space.shape[0]  # 环境信息维度
-    dqn = DQN(N_STATES, N_ACTIONS, MEMORY_CAPACITY, TARGET_UPDATE_STEP, BATCH_SIZE, LR, EPSILON, GAMMA)
-
-    e_reward = 0
-    for episode in range(1, EPISODE + 1):
-        s = env.reset()
-        s = s / 255.0
-        total_reward = 0
-        for step in range(EPISODE_STEP):
-            env.render()
-            a = dqn.choose_action(s)
-
-            # 执行动作
-            s_, r, done, info = env.step(a)
-            s_ = s_ / 255.0
-            dqn.store(s, a, r, s_)
-
-            total_reward += r
-            if dqn.memory_counter > LEARN_START:
-                dqn.learn()
-
-            if done:
-                break
-            s = s_
-        dqn.save()
-        print("episode: ", episode, "Reward: ", total_reward)
-        e_reward += total_reward
-        if episode % 10 == 0:
-            print("total_reward/10: ", e_reward / 10)
-            e_reward = 0
+    n_actions = env.action_space.n  # 动作数量
+    n_states = env.observation_space.shape[0]  # 环境信息维度
+    agent = DQNAgent(n_states, n_actions)
+    train(env, agent)
